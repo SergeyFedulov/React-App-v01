fb79531d8a1b106d96c0d5e640c2f968ede38b8d
Sergey Fedulov <sergeyfedulov.sfd@gmail.com>
2021-12-21 17:18:28 +0300

    feat: add function addPost

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,8 +6,9 @@ const MyPosts = (props) => {
     let postsElements = props.posts.map((post) => <Post message={post.message} likesCount={post.likesCount} />);
     let newPostElement = React.createRef();
     let addPost = () => {
+        debugger;
         let text = newPostElement.current.value;
-        alert(text);
+        props.addPost(text);
     }
     return (
         <div className={classes.postsBlock}>
